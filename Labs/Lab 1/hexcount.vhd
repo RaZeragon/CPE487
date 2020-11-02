d3254f12dd5465104a174550c4d30ceb07ac0775
Kevin Ward <kward3@stevens.edu>
2020-11-02 04:21:45 -0500

    Added Lab 4 and Lab 5

---
+++ Labs/Lab 1/hexcount.vhd
@@ -0,0 +1,40 @@
+-- Author: Kevin Lu
+-- Date: 2/19/2020
+-- Title: hexcount.vhd
+-- Code Version: N/A
+-- Type: Source File
+-- Availability: https://github.com/kevinwlu/dsd/blob/master/Nexys-A7/Lab-1/hexcount.vhd
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY hexcount IS
+	PORT (
+		clk_50MHz : IN STD_LOGIC;
+		anode : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+		seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+	);
+END hexcount;
+
+ARCHITECTURE Behavioral OF hexcount IS
+	COMPONENT counter IS
+		PORT (
+			clk : IN STD_LOGIC;
+			count : OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
+		);
+	END COMPONENT;
+	COMPONENT leddec IS
+		PORT (
+			dig : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+			data : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+			anode : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+			seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+		);
+	END COMPONENT;
+	SIGNAL S : STD_LOGIC_VECTOR (3 DOWNTO 0);
+BEGIN
+	C1 : counter
+	PORT MAP(clk => clk_50MHz, count => S);
+	L1 : leddec
+	PORT MAP(dig => "00", data => S, anode => anode, seg => seg);
+END Behavioral;
